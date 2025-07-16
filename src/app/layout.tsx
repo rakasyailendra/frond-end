c429f6ec4e9b9303132a113f922c9d2a8d7af0bc
rakasyailendra <mrakhasyailendra@gmail.com>
2025-07-17 01:34:08 +0700

    Fix: Corrected integrity attribute in layout.tsx for Vercel build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
-          // PERBAIKI DI SINI: Pastikan atributnya adalah 'integrity', BUKAN 'xintegrity'
           integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
           crossOrigin="anonymous" 
           referrerPolicy="no-referrer" 
